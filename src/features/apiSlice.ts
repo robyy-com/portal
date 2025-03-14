52c9ebfae3c7803bb28294192091212865c1bab6
Utso sarkar <utsosarkar7870@gmail.com>
2025-03-14 23:31:32 +0600

    api problem fixed

--- src/features/apiSlice.ts
+++ src/features/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://api.robyy.com/v1.1.0",
+  baseUrl: "https://api-robyy.s3bd.com/v1.1.0",
   // prepareHeaders: async (headers, { getState }) => {
   //   const data = localStorage.getItem("isAuth");
   //   const token = data ? JSON.parse(data).token : null;
