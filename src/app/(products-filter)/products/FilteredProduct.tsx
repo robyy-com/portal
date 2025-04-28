ac705fde88e66d36b681588b9efe8f81fcc35685
Shafiqbd <shafiq523000@gmail.com>
2025-04-28 23:31:16 +0600

    layout bug fix

--- src/app/(products-filter)/products/FilteredProduct.tsx
+++ src/app/(products-filter)/products/FilteredProduct.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 export default function FilteredProduct({ products }: Props) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 w-full">
       {products &&
         products?.map((item: any) => (
           <ProductCard key={item?.Id} product={item} />
